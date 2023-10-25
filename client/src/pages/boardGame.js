e11314436421690e876c992859dc296a40a93149
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-10-25 10:45:33 +0200

    work in progress

--- client/src/pages/boardGame.js
+++ client/src/pages/boardGame.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ReactDOM } from 'react-dom'
 import Tabs from '../components/TabComp'
 import DecorComp from '../components/DecorComp'
+import RulesContent from '../components/RulesContent'
 const BoardGame = () => {
     return (
         <div>
             Gomoku
             <Tabs />
+            <RulesContent/>
             <DecorComp />
         </div>
     )
