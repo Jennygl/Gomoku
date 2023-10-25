e8509fc4f882532e0ad6cbd6516f1c255d5ebf6b
Jennygl <jenny.grinde@iths.se>
2023-10-25 11:13:53 +0200

    Gomoku page title

--- client/src/pages/boardGame.js
+++ client/src/pages/boardGame.js
@@ -3,11 +3,12 @@ import { ReactDOM } from 'react-dom'
 import Tabs from '../components/TabComp'
 import DecorComp from '../components/DecorComp'
 import NewGameButton from '../components/NewGameButton'
+import GomokuTitle from '../components/GomokuTitle'
 
 const BoardGame = () => {
     return (
         <div>
-            Gomoku
+            <GomokuTitle />
             <Tabs />
             <NewGameButton />
             <DecorComp />
