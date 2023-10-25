38711c6825583547953dbee06fdf6710448d4ff6
dollingadam <adam.dolling1@gmail.com>
2023-10-25 13:18:03 +0200

    Added board, boxes and simple functions for being able to press a square and place a X or O

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import BoardGame from './pages/boardGame'
-
+import './App.css';
 function App() {
+
+
     return (
         <div>
             <h1>Hello, React!</h1>
             <BoardGame />
+
         </div>
     )
 }
--- client/src/pages/boardGame.js
+++ client/src/pages/boardGame.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactDOM } from 'react-dom'
 import BasicExample from '../components/TabComp'
+import BoardComp from '../components/BoardComp'
+import '../App.css'
 const BoardGame = () => {
+    const [board, setBoard] = useState(Array(400).fill(null))
+    const [xPlaying, setXPlaying] = useState(true)
+    const handleBoxClick = (boxIdx) => {
+        const updatedBoard = board.map((value, idx) => {
+            if (idx === boxIdx) {
+                return xPlaying === true ? "X" : "O";
+            } else {
+                return value;
+            }
+        })
+        setBoard(updatedBoard);
+
+        setXPlaying(!xPlaying);
+    }
     return (
         <div>
             Gomoku
             <BasicExample />
+            <BoardComp board={board} onClick={handleBoxClick} />
         </div>
     )
 }
+
 
 export default BoardGame
