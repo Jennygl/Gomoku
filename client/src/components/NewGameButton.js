1ce0e9766a499840f09c5980631998521b24f558
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-10-26 10:02:53 +0200

    updated code for main

--- client/src/components/NewGameButton.js
+++ client/src/components/NewGameButton.js
@@ -11,17 +11,20 @@ const NewGameButton = () => {
 
 export default NewGameButton
 const NEWGAME = styled.div`
-    position: absolute;
-    margin-top: 10px;
-    right: 10vw;
-
     .new-game {
         background-color: rgba(103, 160, 69, 0.46);
-        width: 540px;
+        width: 30vw;
         height: 83px;
         border-radius: 8px;
         font-size: 24px;
         font-family: Inter;
         text-align: center;
+        margin-top: 10px;
+        border: none;
+    }
+    @media (max-width: 992px) {
+        .new-game {
+            width: 80vw;
+        }
     }
 `
