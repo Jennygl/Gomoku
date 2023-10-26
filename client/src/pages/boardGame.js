e4c76ef3ca76e92728ca00d16135d54fb8d24cb2
Jennygl <jenny.grinde@iths.se>
2023-10-26 11:13:07 +0200

    Adjusted tree position

--- client/src/pages/boardGame.js
+++ client/src/pages/boardGame.js
@@ -33,10 +33,9 @@ const BoardGame = () => {
                     <div>
                         <Tabs />
                         <NewGameButton />
+                <DecorComp />
                     </div>
                 </div>
-
-                <DecorComp />
             </Body>
         </div>
     )
