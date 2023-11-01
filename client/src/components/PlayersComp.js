6b2f5a4516fc708fd01395d51f03f791609d1f96
Jennygl <jenny.grinde@iths.se>
2023-11-01 10:16:52 +0100

    Render player name and id. Render cols and rows count

--- client/src/components/PlayersComp.js
+++ client/src/components/PlayersComp.js
@@ -1,52 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
+// import React, { useState, useEffect } from 'react'
+// import styled from 'styled-components'
 
-function Players() {
-    const [data, setData] = useState([])
-    const getData = () => {
-        fetch('../game.json', {
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            }
-        })
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (myJson) {
-                setData(myJson)
-            })
-    }
-    useEffect(() => {
-        getData()
-    }, [])
+// function Players (props) {
 
-    return (
-        <ThisGame>
-            {data &&
-                data.length > 0 &&
-                data.map((item, i) => (
-                    <div key={i}>
-                        <p>
-                            Player 1:{' '}
-                            <strong>
-                                {item.player1.name}, {item.player1.id}
-                            </strong>
-                        </p>
-                        <p>
-                            Player 2:{' '}
-                            <strong>
-                                {item.player2.name}, {item.player2.id}
-                            </strong>
-                        </p>
-                    </div>
-                ))}
-        </ThisGame>
-    )
-}
+//     return (
+//         <ThisGame>
+// <p>{props.boardData.player1.name}</p>
+//         </ThisGame>
+//     )
+// }
 
-export default Players
+// export default Players
 
-const ThisGame = styled.div`
-    margin: 1em;
-`
+// const ThisGame = styled.div`
+//     margin: 1em;
+// `
